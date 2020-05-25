36661ac410db7ddc5c74b3e69808a43817a0f476
Raymond Oung <r.oung@hapi-robo.com>
2020-05-25 13:47:53 +0900

    Updates README and comments

--- README.md
+++ README.md
@@ -1,13 +1,36 @@
 # Jitsi Headless
 
-## Install [Node.js](https://nodejs.org/en/)
-For details, see [here](https://github.com/nodesource/distributions)
+Specifically, running [Jitsi](https://jitsi.org/) on a Raspberry Pi. 
+
+
+## Instructions
+Start with a fresh installation of [Rasbian Buster Lite](https://www.raspberrypi.org/downloads/raspbian/).
+
+Install dependencies:
 ```
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt install -y nodejs
+sudo apt-get update
+sudo apt-get install chromium-browser xvfb
 ```
 
-Check installation:
+Start an Xvfb display:
 ```
-node -v
+Xvfb :1 -screen 0 1920x1080x24 &
 ```
+
+We will point chromium this display with `--display=:1`
+
+Install [Node.js](https://nodejs.org/en/). For details, see [here](https://github.com/nodesource/distributions):
+```
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt install nodejs
+```
+
+Plug in a webcam and run the script:
+```
+node app.js
+```
+
+
+## References
+* [Streaming a Webcam to Jitsi Meet Room](https://code.saghul.net/tag/headless/)
+* [raspberry pi Chromium revision is not downloaded](https://github.com/puppeteer/puppeteer/issues/550#issuecomment-551991273)
--- app.js
+++ app.js
@@ -1,80 +1,81 @@
-const puppeteer = require('puppeteer');
+/*
+Streams the first webcam in the system to the specified Jitsi Meet room. 
+Audio is currently not sent.
 
-// Reference: https://code.saghul.net/tag/headless/
-// Streams the first webcam in the system to the specified Jitsi Meet room. Audio is currently
-// not sent, but it can be easily enabled by disabling the corresponding setting in `meetArgs`.
-//
-// TODO
-//   - Detect if we are kicked from the room
-//   - Support authenticated deployments
-//
-// NOTE: only tested on GNU/Linux.
+TODO
+- Detect if we are kicked from the room
+- Support authenticated deployments
+*/
 
-async function main(room, baseUrl='https://meet.jit.si') {
-    // Chromium command line flags
-    // https://peter.sh/experiments/chromium-command-line-switches/
-    const chromeArgs = [
-        // Disable sandboxing, gives an error on Linux
-        // '--no-sandbox',
-        // '--disable-setuid-sandbox',
-        // Automatically give permission to use media devices
-        '--use-fake-ui-for-media-stream',
-        // Silence all output, just in case
-        '--alsa-output-device=plug:null',
-	'--display=:1',
-	'--no-sandbox',
-	'--disable-extensions',
-    ];
+const puppeteer = require("puppeteer");
 
-    // Jitsi Meet options
-    // https://github.com/jitsi/lib-jitsi-meet/blob/master/doc/API.md
-    // const meetArgs = [
-        // Disable receiving of video
-        // 'config.channelLastN=0',
-        // Mute our audio
-        // 'config.startWithAudioMuted=true',
-        // Don't use simulcast to save resources on the sender (our) side
-        // 'config.disableSimulcast=true',
-        // No need to process audio levels
-        // 'config.disableAudioLevels=true',
-        // Disable P2P mode due to a bug in Jitsi Meet
-        // 'config.p2p.enabled=false'
-    // ];
+async function main(room, baseUrl = "https://meet.jit.si") {
+  // Chromium browser options
+  // https://peter.sh/experiments/chromium-command-line-switches/
+  const chromeArgs = [
+    // Automatically give permission to use media devices
+    "--use-fake-ui-for-media-stream",
 
-    // const url = `${baseUrl}/${room}#${meetArgs.join('&')}`;
-    const url = `${baseUrl}/${room}`;
-    console.log(`Loading ${url}`);
+    // Silence all output, just in case
+    "--alsa-output-device=plug:null",
 
-    // Puppeteer launch options
-    // https://github.com/puppeteer/puppeteer/blob/v3.0.1/docs/api.md#puppeteerlaunchoptions
-    const browser = await puppeteer.launch({
-	defaultViewport: {height: 1080, width: 1920},
-        headless: false, // toggle for debugging
-        args: chromeArgs,
-	executablePath: '/usr/bin/chromium-browser',
-        handleSIGINT: false
-    });
-    const page = await browser.newPage();
+    // Point chromium to xvfb display
+    "--display=:1",
+    "--no-sandbox",
+    "--disable-extensions",
+  ];
 
-    // Manual handling on SIGINT to gracefully hangup and exit
-    process.on('SIGINT', async () => {
-        console.log('Exiting...');
-        await page.evaluate('APP.conference.hangup();');
-        await page.close();
-        browser.close();
-        console.log('Done!');
-        process.exit();
-    });
+  // // Jitsi Meet options
+  // // https://github.com/jitsi/lib-jitsi-meet/blob/master/doc/API.md
+  // const meetArgs = [
+  //   // Disable receiving of video
+  //   "config.channelLastN=0",
+  //   // Mute our audio
+  //   "config.startWithAudioMuted=true",
+  //   // Don't use simulcast to save resources on the sender (our) side
+  //   "config.disableSimulcast=true",
+  //   // No need to process audio levels
+  //   "config.disableAudioLevels=true",
+  //   // Disable P2P mode due to a bug in Jitsi Meet
+  //   "config.p2p.enabled=false",
+  // ];
 
-    await page.goto(url);
+  // const url = `${baseUrl}/${room}#${meetArgs.join("&")}`;
+  const url = `${baseUrl}/${room}`;
+  console.log(`Loading ${url}`);
 
-    // Mute audio
-    await page.evaluate(`APP.conference.muteAudio(true);`);
+  // Puppeteer launch options
+  // https://github.com/puppeteer/puppeteer/blob/v3.0.1/docs/api.md#puppeteerlaunchoptions
+  // https://github.com/puppeteer/puppeteer/issues/550#issuecomment-551991273
+  const browser = await puppeteer.launch({
+    defaultViewport: { height: 1080, width: 1920 },
+    headless: false,
+    args: chromeArgs,
+    executablePath: "/usr/bin/chromium-browser",
+    handleSIGINT: false,
+  });
+  const page = await browser.newPage();
 
-    // Set some friendly display name
-    await page.evaluate(`APP.conference.changeLocalDisplayName('Webcam');`);
+  // Manual handling on SIGINT to gracefully hangup and exit
+  process.on("SIGINT", async () => {
+    console.log("Exiting...");
+    await page.evaluate("APP.conference.hangup();");
+    await page.close();
+    browser.close();
+    console.log("Done!");
+    process.exit();
+  });
 
-    console.log('Running...');
+  // Start Jitsi Meet
+  await page.goto(url);
+
+  // Mute audio
+  await page.evaluate(`APP.conference.muteAudio(true);`);
+
+  // Set some friendly display name
+  await page.evaluate(`APP.conference.changeLocalDisplayName('Webcam');`);
+
+  console.log("Running...");
 }
 
-main(process.argv[2] || 'temi-4164126087');
+main(process.argv[2] || "temi-4164126087");
